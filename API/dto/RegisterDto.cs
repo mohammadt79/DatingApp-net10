608c6ec25282022415f0e78f93406b61dc234d4f
mohammadt <mohmmadt1379@gmail.com>
2026-09-22 14:59:12 +0330

    required dto

--- API/dto/RegisterDto.cs
+++ API/dto/RegisterDto.cs
@@ -2,6 +2,6 @@ namespace API.DTO;
 
 public class RegisterDto
 {
-    public string Username { get; set; } = string.Empty;
-    public string Password { get; set; } = string.Empty;
+    public required string Username { get; set; }
+    public required string Password { get; set; }
 }
