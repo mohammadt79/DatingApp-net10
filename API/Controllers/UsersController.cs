35093929bbb20ddd998bf8d787c1eb00bc5838ac
mohammadt <mohmmadt1379@gmail.com>
2026-09-24 10:37:02 +0330

    allow user controll

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,32 +1,34 @@
-using System;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
+//[Authorize]
 public class UsersController(DataContext context) : BaseApiController
 {
     private readonly DataContext _context = context;
 
     [HttpGet]
+    [AllowAnonymous]
     public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
     {
         var users = await _context.Users.ToListAsync();
-        return users;
         return Ok(users);
     }
 
-    [HttpGet("{Id:int}")] // api/Id/1 
+    [HttpGet("{id:int}")]
     public async Task<ActionResult<AppUser>> GetUser(int id)
     {
-        var user = _context.Users.Find(id);
+        var user = await _context.Users.FindAsync(id);
 
-        if (user == null) return NotFound();
+        if (user == null)
+        {
+            return NotFound();
+        }
 
-        return user;
-
-        //return NotFound();
+        return Ok(user);
     }
 }
